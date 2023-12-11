b1bce737ad13a40b297159db3446fe1b7b31363c
Aleksander2006 <aleksvansanten@gmail.com>
2023-12-11 20:14:57 +0100

    mega update

---
+++ mail_api/app.js
@@ -0,0 +1,143 @@
+const express = require('express'); 
+const cors = require('cors'); 
+const app = express();  
+app.use(express.json()); 
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors({ origin: 'http://127.0.0.1:5500' }));  
+
+//--------------------database------------
+var mysql = require('mysql');
+
+const db = mysql.createConnection ({
+  user:"root",
+  host: "localhost",
+  password: "",
+  database: "database boekingen",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Fout bij het verbinden met de database:', err);
+    process.exit(1);
+  } else {
+    console.log('Succesvol verbonden met de database');
+  }
+});
+
+//--------------------------Mail API-------------------
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'aleksvansanten@gmail.com',
+    pass: 'ujwk mmsj pjvh sapf'
+  }
+});
+
+function createMailOptions(klant) {
+  return {
+
+  from: 'aleksvansanten@gmail.com',
+  to: klant.email,
+  subject: 'Bevestiging van Reservering',
+  text: 
+  
+  `Beste ${klant.voornaam},
+
+  Bedankt voor uw reservering! Hier zijn uw reserveringsgegevens:
+  
+  Verblijfssoort: ${klant.verblijfssoort}
+  Reserveringsdatum: ${klant.reserveringsDatum}
+  Aantal personen: ${klant.aantalPersonen}
+  
+  We kijken ernaar uit u te verwelkomen!
+  
+  Met vriendelijke groet,
+  
+  Aleksander van Santen (Medewerker De Groene Weide)`
+  };
+ }
+ 
+ app.post('/send-email', function(req, res) {
+ 
+  let mailOptions = createMailOptions(req.body);
+ 
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+      res.status(500).send(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent: ' + info.response);
+    }
+  });
+ });
+ 
+ app.listen(5500, function() {
+  console.log('Server is running on port 5500');
+ });
+
+
+//----------------------------- RESTFUL API---------------------------------------------
+
+//---------------------------BOEKING AANMAKEN EN VERSTUREN NAAR DB--------------------
+app.post('/klanten/boekingen/:id', (req, res) => {
+  //const klantId = req.params.id;
+  const postData = {
+    verblijfssoort: req.body.verblijfssoort || null,
+    aankomstEnVertrek: req.body.aankomstEnVertrek || null,
+    aantalPersonen: req.body.aantalPersonen || null,
+    voorkeuren: req.body.voorkeuren || null,
+    voorNaam: req.body.voorNaam || null,
+    achterNaam: req.body.achterNaam || null,
+    tussenVoegsel: req.body.tussenVoegsel || null,
+    phoneNumber: req.body.phoneNumber || null,
+    email: req.body.email || null
+  };
+  
+  
+  var values = [postData.verblijfssoort, postData.aankomstEnVertrek, postData.aantalPersonen, postData.voorkeuren, postData.voorNaam, postData.achterNaam, postData.tussenVoegsel, postData.phoneNumber, postData.email];
+ 
+  var sql = 'INSERT INTO boeking (verblijfssoort, aankomstenvertrek, aantalpersonen, voorkeuren, voornaam, achternaam, tussenvoegsel, phonenumber, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.log(err); // log the error to help identify the issue
+      res.status(500).json({ error: 'Er is een fout opgetreden bij het verwerken van uw verzoek.' });
+      return;
+    }
+    res.send('alles is gelukt!!!!');
+  });
+  });
+
+
+//--------------------------------------BOEKING WIJZIGEN EN STUREN NAAR DB----------------
+//app.patch('/klanten/boekingen/wijzigen/:id', (req, res) => {
+//  const klantId = req.params.id;
+//  const booking = {
+//    klantId: klantId,
+//    verblijfssoort: req.body.verblijfssoort,
+//    aankomstEnVertrek: req.body.aankomstEnVertrek,
+//    aantalPersonen: req.body.aantalPersonen,
+//    voorkeuren: req.body.voorkeuren,
+//    voorNaam: req.body.voorNaam,
+//    achterNaam: req.body.achterNaam,
+//    tussenVoegsel: req.body.tussenVoegsel,
+//    phoneNumber: req.body.phoneNumber,
+//    email: req.body.email
+//  };
+//
+//  const sql = 'UPDATE bookings SET verblijfssoort = ?, aankomstEnVertrek = ?, aantalPersonen = ?, voorkeuren = ?, voorNaam = ?, achterNaam = ?, tussenVoegsel = ?, phoneNumber = ?, email = ? WHERE klantId = ?';
+//
+//  db.query(sql, [booking.verblijfssoort, booking.aankomstEnVertrek, booking.aantalPersonen, booking.voorkeuren, booking.voorNaam, booking.achterNaam, booking.tussenVoegsel, booking.phoneNumber, booking.email, klantId], (err, result) => {
+//    if (err) {
+//      res.status(500).json({ error: 'Er is een fout opgetreden bij het verwerken van uw verzoek.' });
+//      return;
+//    }
+//    res.send('<script>alert("Uw boeking is succesvol gewijzigd!"); window.location.href="/";</script>');
+//  });
+//});
