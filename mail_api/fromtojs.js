b630ef258c4348609a757fbf0197f64c258b7f58
Aleksander2006 <aleksvansanten@gmail.com>
2023-12-15 13:18:18 +0100

    update

--- mail_api/fromtojs.js
+++ mail_api/fromtojs.js
@@ -31,7 +31,6 @@ const vakantieform = document.getElementById('vakantieform')
       'Content-Type': 'application/json'
     },
     body: json
-
   });
 });
 
@@ -97,76 +96,4 @@ const vakantieform = document.getElementById('vakantieform')
 //    body: json
 //
 //  });
-//});
-
-
-//-----------------------VERZOEK VOOR WIJZIGEN BOEKING---------------------
-//const wijzigBoeking = document.getElementById('knop');
-//
-//gegevensform.addEventListener('submit', event => {
-//  event.preventDefault();
-//
-//  var gegevensformData = new FormData(gegevensform);
-//  var vakantieformData = new FormData(vakantieform);
-//  var object = {};
-//
-//  gegevensformData.forEach(function (value, key) {
-//    object[key] = value;
-//  });
-//
-//  vakantieformData.forEach(function (value, key) {
-//    object[key] = value;
-//    
-//  });
-//  var json = JSON.stringify(object);
-//
-//  console.log(json);
-//
-//  const wijzig = fetch("http://localhost:5500/klanten/boekingen/wijzigen/:id", {
-//    method: 'POST',
-//    headers: {
-//      'Content-Type': 'application/json'
-//    },
-//    body: json
-//  });
-//});
-//
-//
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//
-////wijzigBoeking.addEventListener('submit', event => {
-////  event.preventDefault();
-////
-////  var formData = new FormData(form);
-//  var object = {};
-//  formData.forEach(function(value, key){
-//    object[key] = value;
-//  });
-//  var json = JSON.stringify(object);
-//
-//  console.log(json);
-//
-//  fetch('/klanten/boekingen/:id', {
-//    method: 'POST',
-//    headers: {
-//      'Content-Type': 'application/json'
-//    },
-//    body: json
-//  })
 //});
