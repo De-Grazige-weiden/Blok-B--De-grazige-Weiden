d09efd720cb8f4293e3a3f63f49ea096841cc76a
Aleksander2006 <aleksvansanten@gmail.com>
2024-01-08 22:36:48 +0100

    update

--- mail_api/app.js
+++ mail_api/app.js
@@ -90,16 +90,16 @@ function createMailOptions(klant) {
 //----------------KLANT WEBSITE-----------BOEKING AANMAKEN EN VERSTUREN NAAR DB--------------------
 app.post('/api/klanten/boekingen', (req, res) => {
   const postData = {
-    verblijfssoort: req.body.verblijfssoort || null,
-    aankomstDatum: req.body.aankomstDatum || null,
-    vertrekDatum: req.body.vertrekDatum || null,
-    aantalPersonen: req.body.aantalPersonen || null,
-    voorkeuren: req.body.voorkeuren || null,
-    voorNaam: req.body.voorNaam || null,
-    achterNaam: req.body.achterNaam || null,
-    tussenVoegsel: req.body.tussenVoegsel || null,
-    telefoonNummer: req.body.telefoonNummer || null,
-    email: req.body.email || null
+    verblijfssoort: req.body.verblijfssoort,
+    aankomstDatum: req.body.aankomstDatum,
+    vertrekDatum: req.body.vertrekDatum,
+    aantalPersonen: req.body.aantalPersonen,
+    voorkeuren: req.body.voorkeuren,
+    voorNaam: req.body.voorNaam,
+    achterNaam: req.body.achterNaam,
+    tussenVoegsel: req.body.tussenVoegsel,
+    telefoonNummer: req.body.telefoonNummer,
+    email: req.body.email
   };
   
   var values = [postData.verblijfssoort, postData.aankomstDatum, postData.vertrekDatum, postData.aantalPersonen, postData.voorkeuren, postData.voorNaam, postData.achterNaam, postData.tussenVoegsel, postData.telefoonNummer, postData.email];
@@ -138,20 +138,20 @@ app.patch('/api/klanten/boekingen/wijzigen/:id', (req, res) => {
   //const klantId = req.params.id;
   
     let verblijfssoort = req.body.verblijfssoort;
-    let aankomstDatum = req.body.aankomstDatum;
-    let vertrekDatum = req.body.vertrekDatum;
-    let aantalPersonen = req.body.aantalPersonen;
+    let aankomstdatum = req.body.aankomstdatum;
+    let vertrekdatum = req.body.vertrekdatum;
+    let aantalpersonen = req.body.aantalpersonen;
     let voorkeuren = req.body.voorkeuren;
-    let voorNaam = req.body.voorNaam;
-    let achterNaam = req.body.achterNaam;
-    let tussenVoegsel = req.body.tussenVoegsel;
-    let telefoonNummer = req.body.telefoonNummer;
+    let voornaam = req.body.voornaam;
+    let achternaam = req.body.achternaam;
+    let tussenvoegsel = req.body.tussenvoegsel;
+    let telefoonnummer = req.body.telefoonnummer;
     let email = req.body.email;
     let id = req.body.ID;
   
   const sql = 'UPDATE boeking SET verblijfssoort = ?, aankomstdatum = ?, vertrekdatum = ?, aantalpersonen = ?, voorkeuren = ?, voornaam = ?, achternaam = ?, tussenvoegsel = ?, telefoonnummer = ?, email = ? WHERE ID = ?';
 
-  db.query(sql, [verblijfssoort, aankomstDatum, vertrekDatum, aantalPersonen, voorkeuren, voorNaam, achterNaam, tussenVoegsel, telefoonNummer, email, id], (err, result) => {
+  db.query(sql, [verblijfssoort, aankomstdatum, vertrekdatum, aantalpersonen, voorkeuren, voornaam, achternaam, tussenvoegsel, telefoonnummer, email, id], (err, result) => {
     if (err) {
       res.status(500).json({ error: 'Er is een fout opgetreden bij het verwerken van uw verzoek.' });
       return;
