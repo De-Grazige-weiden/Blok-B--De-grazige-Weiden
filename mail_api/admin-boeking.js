0c18eaf751ffd70e9308a0befe044116e6e6b0f3
Aleksander2006 <aleksvansanten@gmail.com>
2024-01-09 10:28:44 +0100

    update

--- mail_api/admin-boeking.js
+++ mail_api/admin-boeking.js
@@ -63,9 +63,29 @@ fetch('http://localhost:5500/api/boekingen')
             <input type="submit" value="Wijzigen">
             <input id="annuleren" type="submit" value="Annuleren">
         </form>
-    </div>
+     </div>
       `;
       container.appendChild(element);
+
+      const button = document.querySelector('#modify') 
+ 
+      button.addEventListener('click', e => { 
+          document.getElementById('modify').style.display = 'none';
+          document.getElementById('cancel').style.display = 'none';
+      }) 
+
+      document.getElementById('modify').addEventListener('click', function() {
+        document.getElementById('bookingForm').style.display = 'block'; // Toon het formulier
+    });
+    
+    document.getElementById('bookingForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+    
+        // Dezelfde code als voorheen om de form data te verwerken
+    
+        document.getElementById('bookingForm').style.display = 'none'; // Verberg het formulier weer
+        document.getElementById('annuleren').style.display = 'none';
+    });
 
     });
   })
@@ -74,25 +94,9 @@ fetch('http://localhost:5500/api/boekingen')
   });
 
 
-const button = document.querySelector('#modify') 
- 
-button.addEventListener('click', e => { 
-	document.getElementById('modify').style.display = 'none';
-    document.getElementById('cancel').style.display = 'none';
-}) 
 
 
-document.getElementById('modify').addEventListener('click', function() {
-    document.getElementById('bookingForm').style.display = 'block'; // Toon het formulier
-});
 
-document.getElementById('bookingForm').addEventListener('submit', function(e) {
-    e.preventDefault();
 
-    // Dezelfde code als voorheen om de form data te verwerken
-
-    document.getElementById('bookingForm').style.display = 'none'; // Verberg het formulier weer
-    document.getElementById('annuleren').style.display = 'none';
-});
 
 
