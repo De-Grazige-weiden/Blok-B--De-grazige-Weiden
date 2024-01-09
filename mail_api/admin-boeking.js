78ea177b4773efa78eb2bf1e380a84bfab52989d
Aleksander2006 <aleksvansanten@gmail.com>
2024-01-09 22:52:50 +0100

    update load

--- mail_api/admin-boeking.js
+++ mail_api/admin-boeking.js
@@ -11,59 +11,64 @@ fetch('http://localhost:5500/api/boekingen')
       let element = document.createElement('div');
       element.setAttribute('data-name', data.name);
       element.innerHTML = `
-        <div class="booking">
-        <h2>Boeking ${data.ID} </h2>
-        <p>Gegevens:<b> Voornaam:${data.voornaam} – <b>Tussenvoegsel:<b> ${data.tussenvoegsel} – <b>Achternaam:<b> ${data.achternaam} – <b>Telefoonnummer:<b> ${data.telefoonnummer} – <b>Email:<b> ${data.email} </p>
-        <p><b>Verblijfssoort:<b> ${data.verblijfssoort} – <b>Aankomstdatum:<b> ${data.aankomstdatum} <b>Vertrekdatum:<b> ${data.vertrekdatum} – <b>Aantal personen:<b> ${data.aantalpersonen} <b>ID:<b> ${data.ID} </p>
-        <button id="modify" class="modify">Wijzigen</button>
-        <button id="cancel" class="cancel">Annuleren</button> 
+      <div class="booking">
+      <h2>Boeking ${data.ID} </h2>
+      <p>Gegevens:<b> Voornaam:${data.voornaam} – <b>Tussenvoegsel:<b> ${data.tussenvoegsel} – <b>Achternaam:<b> ${data.achternaam} – <b>Telefoonnummer:<b> ${data.telefoonnummer} – <b>Email:<b> ${data.email} </p>
+      <p><b>Verblijfssoort:<b> ${data.verblijfssoort} – <b>Aankomstdatum:<b> ${data.aankomstdatum} <b>Vertrekdatum:<b> ${data.vertrekdatum} – <b>Aantal personen:<b> ${data.aantalpersonen} <b>ID:<b> ${data.ID} </p>
+      <button id="modify" class="modify">Wijzigen</button>
+      <button id="cancel" class="cancel">Annuleren</button> 
 
-        <form id="bookingForm">
-            <label>
-                Verblijfssoort:
-                <input type="text" id="verblijfssoort" name="verblijfssoort">
-            </label>
-            <label>
-                Aankomst Datum:
-                <input type="date" id="aankomstDatum" name="aankomstDatum">
-            </label>
-            <label>
-                Vertrek Datum:
-                <input type="date" id="vertrekDatum" name="vertrekDatum">
-            </label>
-            <label>
-                Aantal Personen:
-                <input type="number" id="aantalPersonen" name="aantalPersonen">
-            </label>
-            <label>
-                Voorkeuren:
-                <input type="text" id="voorkeuren" name="voorkeuren">
-            </label>
-            <label>
-                Voornaam:
-                <input type="text" id="voorNaam" name="voorNaam">
-            </label>
-            <label>
-                Achternaam:
-                <input type="text" id="achterNaam" name="achterNaam">
-            </label>
-            <label>
-                Tussenvoegsel:
-                <input type="text" id="tussenVoegsel" name="tussenVoegsel">
-            </label>
-            <label>
-                Telefoonnummer:
-                <input type="tel" id="phoneNumber" name="phoneNumber">
-            </label>
-            <label>
-                Email:
-                <input type="email" id="email" name="email">
-            </label>
-            
-            <input type="submit" value="Wijzigen">
-            <input id="annuleren" type="submit" value="Annuleren">
-        </form>
-     </div>
+      <form id="bookingForm">
+          <label>
+              Verblijfssoort:
+              <input type="text" id="verblijfssoort" name="verblijfssoort">
+          </label>
+          <label>
+              Aankomst Datum:
+              <input type="date" id="aankomstDatum" name="aankomstdatum">
+          </label>
+          <label>
+              Vertrek Datum:
+              <input type="date" id="vertrekDatum" name="vertrekdatum">
+          </label>
+          <label>
+              Aantal Personen:
+              <input type="number" id="aantalPersonen" name="aantalpersonen">
+          </label>
+          <label>
+              Voorkeuren:
+              <input type="text" id="voorkeuren" name="voorkeuren">
+          </label>
+          <label>
+              Voornaam:
+              <input type="text" id="voorNaam" name="voornaam">
+          </label>
+          <label>
+              Achternaam:
+              <input type="text" id="achterNaam" name="achternaam">
+          </label>
+          <label>
+              Tussenvoegsel:
+              <input type="text" id="tussenVoegsel" name="tussenvoegsel">
+          </label>
+          <label>
+              Telefoonnummer:
+              <input type="tel" id="phoneNumber" name="phonenumber">
+          </label>
+          <label>
+              Email:
+              <input type="email" id="email" name="email">
+          </label>
+          <label>
+              ID:
+              <input type="number" id="id" name="ID">
+          </label>
+          
+          
+          <input type="submit" value="Wijzigen">
+          <input id="annuleren" type="submit" value="Annuleren">
+      </form>
+   </div>
       `;
       container.appendChild(element);
 
