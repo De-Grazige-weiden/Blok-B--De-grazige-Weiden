d88eaa5d644131543c4f619e890c9199cc383f69
Aleksander2006 <aleksvansanten@gmail.com>
2024-01-16 11:04:12 +0100

    update

--- admin_website/medewerkermain.js
+++ admin_website/medewerkermain.js
@@ -1,13 +1,22 @@
 document.onreadystatechange = function() {
-    if (document.readyState !== "complete") {
-        document.querySelector(
-          "body").style.visibility = "hidden";
-        document.querySelector(
-          "#spinner").style.visibility = "visible";
-    } else {
-        document.querySelector(
-          "#spinner").style.display = "none";
-        document.querySelector(
-          "body").style.visibility = "visible";
-    }
+  if (document.readyState !== "complete") {
+      document.querySelector(
+        "body").style.visibility = "hidden";
+      document.querySelector(
+        "#spinner").style.visibility = "visible";
+  } else {
+      document.querySelector(
+        "#spinner").style.display = "none";
+      document.querySelector(
+        "body").style.visibility = "visible";
+  }
 };
+
+
+
+
+
+
+
+
+
