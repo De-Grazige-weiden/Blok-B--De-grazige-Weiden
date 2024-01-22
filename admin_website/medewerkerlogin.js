7172a70ef6cbc11cafdd0772c43004bd37e63aea
Aleksander2006 <aleksvansanten@gmail.com>
2024-01-22 12:42:47 +0100

    update

--- admin_website/medewerkerlogin.js
+++ admin_website/medewerkerlogin.js
@@ -1,36 +1,28 @@
 //--------------------LOGIN----------------------------
 
-//const form = document.querySelector(".login")
-//
-//form.addEventListener("submit",(e)=>{
-//    e.preventDefault()
-//
-//    const email = form.email.value
-//    const password = form.password.value
-//
-//    const authenticated = authentication(email,password)
-//
-//    if(authenticated){
-//        window.location.href = "medewerkerboekingen.html"
-//    }else{
-//        alert("wrong")
-//    }
-//})
-//
-//// function for checking username and password
-//
-//function authentication(email,password){
-//    if(email === "bert.grazeweide@gmail.nl" && password === "grazeweide"){
-//        return true
-//    }else{
-//        return false
-//    }
-//}
+const form = document.querySelector(".login")
 
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
 
+    const email = form.email.value
+    const password = form.password.value
 
+    const authenticated = authentication(email,password)
 
+    if(authenticated){
+        window.location.href = "medewerkerboekingen.html"
+    }else{
+        alert("wrong")
+    }
+})
 
+// function for checking username and password
 
-
-
+function authentication(email,password){
+    if(email === "bert.grazeweide@gmail.nl" && password === "grazeweide"){
+        return true
+    }else{
+        return false
+    }
+}
--- mail_api/app.js
+++ mail_api/app.js
@@ -175,26 +175,3 @@ app.delete('/api/klanten/boekingen/verwijderen/:id', (req, res) => {
   });
 });
 
-
-
-
-//----------------login--------------------------
-app.post("/", function(req,res){
-  var username = req.body.email;
-  var password = req.body.password;
-
-  connection.query("select * from loginuser where user_name = ? and user_pass = ?",[username,password],function(error,results,fields){
-      if (results.length > 0) {
-          res.redirect("/medewerkerboekingen.html");
-      } else {
-          res.redirect("/");
-      }
-      res.end();
-  })
-})
-
-// when login is success
-app.get("/welcome",function(req,res){
-  res.sendFile(__dirname + "/medewerkerlogin.html")
-})
-
